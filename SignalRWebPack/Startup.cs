f54493a84f1b86cdc920d6430e0e2390d0f4b1b0
Sinan.Nar <Sinan.Nar@soeidental.com>
2018-10-12 10:12:55 +1300

    created chathub and enabled signalr in startup

--- SignalRWebPack/Startup.cs
+++ SignalRWebPack/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRWebPack.Hubs;
 
 namespace SignalRWebPack
 {
@@ -15,6 +16,7 @@ namespace SignalRWebPack
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,13 @@ namespace SignalRWebPack
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseSignalR(options =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                options.MapHub<ChatHub>("/hub");
             });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
