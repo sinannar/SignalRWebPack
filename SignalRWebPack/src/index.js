72bf15a8371d8f176a95187e9c7eb7e5e298a7f0
Sinan.Nar <Sinan.Nar@soeidental.com>
2018-10-12 10:16:16 +1300

    npm run install and debug without debugging and app is working

--- SignalRWebPack/src/index.js
+++ SignalRWebPack/src/index.js
@@ -1,10 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./css/main.css");
+var signalR = require("@aspnet/signalr");
 var divMessages = document.querySelector("#divMessages");
 var tbMessage = document.querySelector("#tbMessage");
 var btnSend = document.querySelector("#btnSend");
 var username = new Date().getTime();
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .build();
+connection.start().catch(function (err) { return document.write(err); });
+connection.on("messageReceived", function (username, message) {
+    var m = document.createElement("div");
+    m.innerHTML =
+        "<div class=\"message__author\">" + username + "</div><div>" + message + "</div>";
+    divMessages.appendChild(m);
+    divMessages.scrollTop = divMessages.scrollHeight;
+});
 tbMessage.addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
         send();
@@ -12,4 +24,6 @@ tbMessage.addEventListener("keyup", function (e) {
 });
 btnSend.addEventListener("click", send);
 function send() {
+    connection.send("newMessage", username, tbMessage.value)
+        .then(function () { return tbMessage.value = ""; });
 }
--- SignalRWebPack/src/index.ts
+++ SignalRWebPack/src/index.ts
@@ -1,9 +1,26 @@
 ﻿import "./css/main.css";
+import * as signalR from "@aspnet/signalr";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 const username = new Date().getTime();
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .build();
+
+connection.start().catch(err => document.write(err));
+
+connection.on("messageReceived", (username: string, message: string) => {
+    let m = document.createElement("div");
+
+    m.innerHTML =
+        `<div class="message__author">${username}</div><div>${message}</div>`;
+
+    divMessages.appendChild(m);
+    divMessages.scrollTop = divMessages.scrollHeight;
+});
 
 tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
     if (e.keyCode === 13) {
@@ -14,4 +31,6 @@ tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
 btnSend.addEventListener("click", send);
 
 function send() {
+    connection.send("newMessage", username, tbMessage.value)
+        .then(() => tbMessage.value = "");
 }
